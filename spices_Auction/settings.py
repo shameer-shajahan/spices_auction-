62ebc410415ffb473535d028c525a91913dcdfe1
shameer shajahan <shameershajahan17@gmail.com>
2025-06-10 13:38:18 +0530

    Update settings.py

--- spices_Auction/settings.py
+++ spices_Auction/settings.py
@@ -150,5 +150,3 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True  # TLS - Transport Layer Security
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'thespiceproject.dev@gmail.com'
-EMAIL_HOST_PASSWORD = 'ahph yzxx rccb ugde '  
